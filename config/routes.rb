cfd9470479c863bd3870660b5eb9155b8ea53701
Elisabet Ramirez <elisaram@gmail.com>
2023-10-26 15:52:47 +0200

    add Plant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :plant_cares
+  resources :plants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
